bcc115b614fcb4cd32ef289e6c9d509f1c659d03
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2026-02-09 19:16:01 +0100

    Add ProductRepository and extend CampaignRepository with product-related queries

--- campaign-manager/src/main/java/pl/futurum/campaignmanager/campaign/repository/CampaignRepository.java
+++ campaign-manager/src/main/java/pl/futurum/campaignmanager/campaign/repository/CampaignRepository.java
@@ -3,5 +3,10 @@ package pl.futurum.campaignmanager.campaign.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.futurum.campaignmanager.account.model.Campaign;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+    List<Campaign> findAllByProductId(Long productId);
+    Optional<Campaign> findByIdAndProductId(Long id, Long productId);
 }
